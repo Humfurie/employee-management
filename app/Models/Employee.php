916cc07a8124f9f00e304c16d4cd09f52c0853d1
Humphrey Singculan <Humfurie@gmail.com>
2023-03-02 13:25:59 +0800

    adding migrations and models
    Signed-off-by: Humphrey Singculan <Humfurie@gmail.com>

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -4,8 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Employee extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'first_name',
+        'middle_name',
+        'last_name',
+    ];
+
+  /**
+   * Get all of the comments for the EmployeeController
+   *
+   * @return \Illuminate\Database\Eloquent\Relations\HasMany
+   */
+  public function position(): HasMany
+  {
+      return $this->hasMany(Position::class, 'employee_position');
+  }
 }
